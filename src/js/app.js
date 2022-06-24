3f0a4dfa48d5bf44d9d3e1a635cbeba3d9c12781
Grinch3214 <scorpiomax90@gmail.com>
2022-06-24 20:46:04 +0300

    adding header

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,6 @@ import * as flsFunctions from './modules/functions.js';
 flsFunctions.isWebp();
 
 import { animationNavbar,  animationWindow  } from './modules/globalAnimation.js';
+animationNavbar();
 animationWindow();
 
--- src/js/modules/globalAnimation.js
+++ src/js/modules/globalAnimation.js
@@ -1,7 +1,7 @@
 // Animations
 
 function animationNavbar() {
-	const navbar = document.querySelector('.navbar');
+	const navbar = document.querySelector('.header__navbar');
 	let anim = setTimeout(function() {
 		navbar.classList.add('active');
 		clearTimeout(anim);
