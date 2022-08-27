c87dd49f947fd62f29bb08bab3f927009d86cb30
Grinch3214 <scorpiomax90@gmail.com>
2022-08-27 15:19:21 +0300

    fix little bugs on markup and js code described

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,16 @@
 import * as flsFunctions from './modules/functions.js';
-import { animationNavbar,  animationWindow, scrollOptions, footerAccordionOnMobile } from './modules/globalAnimation.js';
-import brackpoints from './modules/brackpoints.js'
+import { locomotiveScrollAnimation, animationNavbar, footerAccordionOnMobile } from './modules/globalAnimation.js';
 import { tabsOnSwiper } from './modules/tabs.js';
 import burgerMenu from './modules/burger.js';
-// import footerAccordionOnMobile from './modules/footer.js'
 
 flsFunctions.isWebp();
 // burger
 burgerMenu();
+
 // animation
+locomotiveScrollAnimation();
 animationNavbar();
-animationWindow();
-// brackpoints
-brackpoints();
+
 // tabs
 tabsOnSwiper();
 
--- src/js/modules/globalAnimation.js
+++ src/js/modules/globalAnimation.js
@@ -1,26 +1,93 @@
 // Animations
 import LocomotiveScroll from 'locomotive-scroll';
-const mediaQuery = window.matchMedia('(min-width: 1310px)');
 
-let scrollOptions = new LocomotiveScroll({
-	el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-	smartphone: {
-		smooth: true
-	},
-	tablet: {
+function locomotiveScrollAnimation() {
+	//? ----- init Locomotive scroll -----
+	let scrollOptions = new LocomotiveScroll({
+		el: document.querySelector('[data-scroll-container]'),
 		smooth: true,
-	},
-	reloadOnContextChange: true,
-});
+		smartphone: {
+			smooth: true
+		},
+		tablet: {
+			smooth: true,
+		},
+		reloadOnContextChange: true,
+	});
 
-setTimeout(locomotiveHeightBug, 3500);
-function locomotiveHeightBug(){
-  window.dispatchEvent(new Event('resize'));
-}
+	//! This timeout and observe need for reset calculate height!!!
+	setTimeout(locomotiveHeightBug, 3500);
+	function locomotiveHeightBug(){
+		window.dispatchEvent(new Event('resize'));
+	};
+	
+	new ResizeObserver(() => scrollOptions.update()).observe(document.querySelector("[data-scroll-container]"));
 
-new ResizeObserver(() => scrollOptions.update()).observe(document.querySelector("[data-scroll-container]"));
+	//? ----- animation on main page for circle background -----
+	function animationWindow() {
+		const circle = document.querySelector('.circle');
+	
+		scrollOptions.on('scroll', (obj) => {
+			let anim = Math.floor(obj.delta.y / 14);
+			circle.style = `
+				transform: rotate(-${anim}deg);
+				transition: all .3s ease;
+			`;
+		});
+	
+		//? ----- calculate :before circle for resize -----
+		const animateRoot = document.styleSheets[0];
+		animateRoot.insertRule(`:root{
+			--a-width:calc(105vw + ${window.innerWidth}px);
+			--a-heigth:calc(180vh + ${window.innerHeight}px);
+		}`);
+	};
+	animationWindow();
 
+	//? ------ change data atributes for main page -----
+	function changeDataAtributesForMainPage() {
+		const stickyBlock = document.querySelector('.power__image-box');
+		const cardsAnimation = document.querySelectorAll('.gaming__card');
+		const partnersAnimation = document.querySelectorAll('.partners__companies > li');
+	
+		function removeDataScrollSpeed(removeDataAnim) {
+			for (let i = 0; i < removeDataAnim.length; i++) {
+				removeDataAnim[i].dataset.scrollSpeed = 0;
+			}
+		};
+	
+		function changingAttributes() {
+			if (window.innerWidth <= 1310) {
+				stickyBlock.removeAttribute('data-scroll-target');
+				stickyBlock.removeAttribute('data-scroll-sticky');
+				stickyBlock.removeAttribute('data-scroll');
+				removeDataScrollSpeed(cardsAnimation);
+				removeDataScrollSpeed(partnersAnimation);
+			} else {
+				stickyBlock.setAttribute('data-scroll-target', '#fixed-elements')
+				stickyBlock.setAttribute('data-scroll-sticky', '');
+				stickyBlock.setAttribute('data-scroll', '');
+	
+				cardsAnimation[0].dataset.scrollSpeed = 3;
+				cardsAnimation[1].dataset.scrollSpeed = 2;
+				cardsAnimation[2].dataset.scrollSpeed = -3;
+	
+				partnersAnimation[0].dataset.scrollSpeed = 2;
+				partnersAnimation[1].dataset.scrollSpeed = 1;
+				partnersAnimation[2].dataset.scrollSpeed = -1;
+				partnersAnimation[3].dataset.scrollSpeed = -2;
+			}
+		};
+		changingAttributes();
+	
+		window.addEventListener('resize', () => {
+			changingAttributes();
+		});
+	};
+	changeDataAtributesForMainPage();
+};
+
+//? ----- for animation header -----
 function animationNavbar() {
 	const navbar = document.querySelector('.header__navbar');
 	let anim = setTimeout(function() {
@@ -28,6 +95,31 @@ function animationNavbar() {
 		clearTimeout(anim);
 	}, 150)
 };
+
+//? ----- footer tabs -----
+function footerAccordionOnMobile() {
+  const accordion = document.querySelectorAll('.footer__title');
+
+	function accordionOnMobile() {
+    accordion.forEach((elem) => {
+      elem.addEventListener("click", function (event) {
+        if (window.innerWidth <= 768) {
+					this.classList.toggle('active');
+          let list = this.nextElementSibling;
+          if (!list.style.maxHeight) {
+						list.style.maxHeight = list.scrollHeight + "px";
+          } else {
+						list.style.maxHeight = null;
+					}
+        }
+      });
+    });
+  }
+  accordionOnMobile();
+};
+
+export { locomotiveScrollAnimation, animationNavbar, footerAccordionOnMobile };
+
 // в этой функции по скролу можем отслеживать активное окно и добавлять классы
 // для каких-то css эффектов UPD: не актуальна при локомотиве :(
 
@@ -70,77 +162,3 @@ function animationNavbar() {
 
 // 	})
 // };
-
-function animationWindow() {
-	const circle = document.querySelector('.circle');
-
-	scrollOptions.on('scroll', (obj) => {
-		let anim = Math.floor(obj.delta.y / 14);
-		// console.log(anim)
-		circle.style = `
-			transform: rotate(-${anim}deg);
-			transition: all .3s ease;
-		`;
-	});
-
-	const animateRoot = document.styleSheets[0];
-	animateRoot.insertRule(`:root{
-		--a-width:calc(105vw + ${window.innerWidth}px);
-		--a-heigth:calc(180vh + ${window.innerHeight}px);
-	}`);
-};
-
-//footer tabs
-function footerAccordionOnMobile() {
-  const accordion = document.querySelectorAll('.footer__title');
-
-	function accordionOnMobile() {
-    accordion.forEach((elem) => {
-      elem.addEventListener("click", function (event) {
-        if (window.innerWidth <= 768) {
-					this.classList.toggle('active');
-          let list = this.nextElementSibling;
-          if (!list.style.maxHeight) {
-						list.style.maxHeight = list.scrollHeight + "px";
-          } else {
-						list.style.maxHeight = null;
-					}
-        }
-      });
-    });
-  }
-  accordionOnMobile();
-};
-
-// function brackpoints() {
-// 	const stickyBlock = document.querySelector('.power__image-box');
-// 	const cardsAnimation = document.querySelectorAll('.gaming__card');
-
-// 	function removeDataScrollSpeed() {
-// 		for (let i = 0; i < cardsAnimation.length; i++) {
-// 			cardsAnimation[i].dataset.scrollSpeed = 0
-// 		}
-// 	}
-
-// 	if (window.innerWidth <= 1310 && stickyBlock.getAttribute('data-scroll-target')) {
-// 		stickyBlock.removeAttribute('data-scroll-target');
-// 		stickyBlock.removeAttribute('data-scroll-sticky');
-// 		removeDataScrollSpeed();
-// 	}
-// 	window.addEventListener('resize', function() {
-// 		if (window.innerWidth <= 1310) {
-// 			stickyBlock.removeAttribute('data-scroll-target');
-// 			stickyBlock.removeAttribute('data-scroll-sticky');
-			
-// 			removeDataScrollSpeed();
-// 		} else {
-// 			stickyBlock.setAttribute('data-scroll-target', '#fixed-elements')
-// 			stickyBlock.setAttribute('data-scroll-sticky', '');
-
-// 			cardsAnimation[0].dataset.scrollSpeed = 3;
-// 			cardsAnimation[1].dataset.scrollSpeed = 2;
-// 			cardsAnimation[2].dataset.scrollSpeed = -3;
-// 		}
-// 	})
-// }
-export { scrollOptions, animationNavbar,  animationWindow, footerAccordionOnMobile };
